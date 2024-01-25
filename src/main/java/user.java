789173f2a4eea1e885861553c1637c4a2ab58d41
firstmeanic <arasheparsanezhad@gmail.com>
2024-01-25 22:04:19 +0330

    add getter and setter method for all fields

--- src/main/java/user.java
+++ src/main/java/user.java
@@ -13,4 +13,36 @@ public class user {
       this.usename = usename;
       this.password = password;
    }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getUsename() {
+      return usename;
+   }
+
+   public void setUsename(String usename) {
+      this.usename = usename;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
 }
