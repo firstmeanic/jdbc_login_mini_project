2845f958fe05c4af670e017091293c756a7b6400
firstmeanic <arasheparsanezhad@gmail.com>
2024-01-25 22:02:50 +0330

    add no args and all args constructor

--- src/main/java/user.java
+++ src/main/java/user.java
@@ -4,4 +4,13 @@ public class user {
    private String usename;
    private String password;
 
+   public user() {
+   }
+
+   public user(String firstName, String lastName, String usename, String password) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.usename = usename;
+      this.password = password;
+   }
 }
