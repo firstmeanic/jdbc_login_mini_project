c580e31ca9a71ba2bdaa29ba6c8de4a19cac3c1f
firstmeanic <arasheparsanezhad@gmail.com>
2024-01-27 14:54:39 +0330

    add commands to knows the user requirement

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Menu {
+    Scanner scanner=new Scanner(System.in);
+    public void publicMenu(){
+        System.out.println(" ***** WELCOME ***** ");
+        System.out.println("1- SIGN UP");
+        System.out.println("2- SIGN IN");
+        System.out.println("3- EXIT");
+        System.out.println("choose your option: ");
+        int number=scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(number);
+    }
 }
