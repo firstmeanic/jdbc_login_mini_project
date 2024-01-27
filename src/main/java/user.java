c4ad7f1518bde0efeb47597b5bec45127ebf51fd
firstmeanic <arasheparsanezhad@gmail.com>
2024-01-27 19:35:23 +0330

    add new constuctor without id

--- src/main/java/user.java
+++ src/main/java/user.java
@@ -1,4 +1,5 @@
 public class user {
+   private int id;
    private String firstName;
    private String lastName;
    private String usename;
@@ -7,11 +8,27 @@ public class user {
    public user() {
    }
 
+   public user(int id, String firstName, String lastName, String usename, String password) {
+      this.id = id   ;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.usename = usename;
+      this.password = password;
+   }
+
    public user(String firstName, String lastName, String usename, String password) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.usename = usename;
       this.password = password;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
    }
 
    public String getFirstName() {
@@ -49,10 +66,12 @@ public class user {
    @Override
    public String toString() {
       return "user{" +
-              "firstName='" + firstName + '\'' +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
               ", lastName='" + lastName + '\'' +
               ", usename='" + usename + '\'' +
               ", password='" + password + '\'' +
               '}';
    }
 }
+
