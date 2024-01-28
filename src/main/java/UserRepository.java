b4059b2a41e4bb467954a448d14116ac69385694
firstmeanic <arasheparsanezhad@gmail.com>
2024-01-28 17:16:22 +0330

    add registerUser method

--- src/main/java/UserRepository.java
+++ src/main/java/UserRepository.java
@@ -1,2 +1,24 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserRepository {
+    JdbcConnection jdbcConnection=new JdbcConnection();
+
+    public UserRepository() throws SQLException {
+    }
+
+    public int registerUser(User user) throws SQLException {
+        Connection connection=jdbcConnection.getConnection();
+        String addUser="INSERT INTO users(first_name, last_name, username, password)VALUES(?,?,?,?);";
+        PreparedStatement preparedStatement=connection.prepareStatement(addUser);
+        preparedStatement.setString(1,user.getFirstName());
+        preparedStatement.setString(2,user.getLastName());
+        preparedStatement.setString(3,user.getUsername());
+        preparedStatement.setString(4,user.getPassword());
+        int result=preparedStatement.executeUpdate();
+        return result;
+
+    }
+
 }
